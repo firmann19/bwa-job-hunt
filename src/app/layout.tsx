7499054757bd2ceb40d637ffa3ec8082551a8c90
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-09-30 18:17:14 +0700

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Epilogue } from 'next/font/google'
+import Image from "next/image"
 
-const inter = Inter({ subsets: ['latin'] })
+const epilogue = Epilogue({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${epilogue.className} relative overflow-x-hidden`}>
+        <div>Navbar</div>
+        <main>
+          <div className='w-full h-screen absolute top-0 -z-10' />
+          <div className='absolute w-2/3 h-screen top-0 right-0 -z-10'>
+            <Image src="/images/pattern.png" alt="/images/pattern.png" fill />
+          </div>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
